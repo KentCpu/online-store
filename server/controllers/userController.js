ad875c66a87cae6fb139b48d893d34415b8416b1
Kent-cpu <74884403+Kent-cpu@users.noreply.github.com>
2022-08-21 19:01:33 +0700

    Added authorization jwt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,63 @@
 
 const ApiError = require("../error/ApiError");
+const uuid = require("uuid");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateJwt = (id, email, role) => {
+    return jwt.sing(
+        { id, email, role },
+        process.env.SECRET_KEY,
+        { expiresIn: "24h" }
+    );
+}
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        try {
+            const { name, nickname, email, password, role } = req.body;
+
+            const candidate = await User.findOne({ email: email });
+            if (candidate) {
+                return next(ApiError.badRequest("Пользователь с таким email уже существует"));
+            }
+
+            const hashPassword = await bcrypt.hashPassword(password, 5);
+            const user = new User({ email, hashPassword, name, nickname, role })
+            const token = generateJwt(user.id, user.email, user.role);
+
+            return token.json({ token });
+
+            // const { img } = req.files;
+            // let fileName = uuid.v4() + ".jpg";
+            // img.mv(path.resolve(__dirname, "..", "static", fileName));
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
 
     }
 
     async login(req, res) {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return next(ApiError.internal("Пользователь не найден"));
+        }
 
+        let comparePassword = bcrypt.compareSync(password, user.password);
+        if (!comparePassword) {
+            return next(ApiError.internal("Указан неверный пароль"));
+        }
+
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
     }
 
     async check(req, res, next) {
-        const { id } = req.query;
-        if (!id) {
-            return next(ApiError.badRequest("Не задан ID"));
-        }
-        res.json(id);
+        const token = generateJwt(req.user.id, req.user.email, req.user.role);
+        return res.json({ token });
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 // Обработка ошибок, последний middleware

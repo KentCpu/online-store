75898c47b119bfb91c5dc3e1d8ef0dbd2b583643
Kent-cpu <74884403+Kent-cpu@users.noreply.github.com>
2022-10-13 12:21:48 +0700

    Added picture avatar

--- client/src/components/Header/Profile.tsx
+++ client/src/components/Header/Profile.tsx
@@ -2,11 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import { Link } from "react-router-dom";
-import { PROFILE_ROUTE } from "../../utils/constants/url";
+import { PROFILE_ROUTE, SERVER_URL } from "../../utils/constants/url";
 import s from "./Header.module.scss";
 
 export const Profile = () => {
-    const isAuth = useTypedSelector(state => state?.user.isAuth);
+    const isAuth = useTypedSelector(state => state.user?.isAuth);
+    const avatar = useTypedSelector(state => state.user?.userData?.avatar);
 
     if (!isAuth) {
         return <></>;
@@ -15,7 +16,12 @@ export const Profile = () => {
     return (
         <div className={s["profile"]}>
             <Link className={s["profile__link"]} to={PROFILE_ROUTE}>
-                <FontAwesomeIcon icon={faUser} />
+                {
+                    avatar ?
+                        <img className={s["profile__img"]} src={`${SERVER_URL}/${avatar}`} alt="Profile" />
+                        :
+                        <FontAwesomeIcon icon={faUser} />
+                }
             </Link>
         </div>
     );
